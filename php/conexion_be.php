1d3dcdb8a8e3eef822e35a43adc52a1dc881eb48
MasterZ <155936519+CarlosRivera2025@users.noreply.github.com>
2025-01-14 01:58:40 -0600

    PHP Inicio

---
+++ php/conexion_be.php
@@ -0,0 +1,13 @@
+<?php
+
+
+    $conexion = mysqli_connect("localhost", "root", "", "login_register_db");
+
+    if ($conexion) {
+        echo 'Conexión exitosa';
+    }else {
+        echo 'Conexión fallida';
+    }
+
+
+?>
